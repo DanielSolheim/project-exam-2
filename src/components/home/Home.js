1a4200526aa89c5dcc1c167df65414ceb81bcd87
DanielSolheim <danielsolheim1@gmail.com>
2020-05-09 17:23:48 +0200

    progress Home and menu

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,7 +3,26 @@ import React from "react";
 export default function Home (){
   return (
     <div className="Home">
-      Hello from Home
+      <div className="home--content center-container">
+
+         <section className="home--form">
+               <h1> Search for places to stay in Bergen </h1>
+               <form className="home--form__section">
+                 <div>
+                   <input type="text" placeholder="search for hotel" name="search" />
+                 </div>
+                 <div>
+                  <input type="date"  placeholder="checkinn" name="check-out" />
+                 </div>
+                 <div>
+                  <input type="date"  placeholder="checkout" name="check-inn" />
+                 </div>
+                 <div>
+                  <input type="submit"  name="submit" />
+                 </div>
+               </form>
+          </section>
+      </div>
     </div>
   )
 }
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -15,9 +15,10 @@ export default function Layout() {
    return (
     <Router>
      <div className="Layout">
+      <div className="header--container">
        <header className="main--header">
          <div className="logo">
-           <img src="./images/logo.svg" alt="logo" />
+           <img src="./images/logo-01.png" alt="logo" />
          </div>
          <nav className="main--menu">
            <NavLink className="main--menu__link" to="/"> Home </NavLink>
@@ -25,11 +26,14 @@ export default function Layout() {
            <NavLink className="main--menu__link" to="/admin"> Admin </NavLink>
          </nav>
        </header>
+      </div>
+      <div className="container">
        <Switch>
              <Route path="/" exact component={Home}/>
              <Route path="/contact" component={Contact} />
              <Route path="/admin" component={Admin} />
       </Switch>
+      </div>  
      </div>
     </Router>
    );
