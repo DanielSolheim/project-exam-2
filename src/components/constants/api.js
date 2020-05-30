fd8c524957f4799f56f869f2703c074b742b62f5
DanielSolheim <danielsolheim1@gmail.com>
2020-05-30 16:41:18 +0200

    back

--- src/components/constants/api.js
+++ src/components/constants/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000/hotel-api/establishmedsadsasnts.json";
-export const CONTACT_URL = "http://localhost:3000/hotel-api/contactdasdas.json";
+export const BASE_URL = "http://localhost:3000/hotel-api/establishments.json";
+export const CONTACT_URL = "http://localhost:3000/hotel-api/contacts.json";
