b0c80be34ffa8ffe5fbbfc8229df99dfef347e0c
DanielSolheim <danielsolheim1@gmail.com>
2020-06-05 22:41:38 +0200

    mobil

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## Exam Front end Daniel Solheim
+This is my ending exam for front end development at Noroff School of Technology and Media
+
+#Instructions
+
+The site is live on netlify:
+https://kind-swanson-f2c39c.netlify.app/
--- src/components/constants/api.js
+++ src/components/constants/api.js
@@ -5,8 +5,9 @@ export const CONTACT_URL = "https://dansol.no/hotel-api/contact.json";
 export const ENQUIRIES_URL = "https://dansol.no/hotel-api/enquiries.json";
 
 
+
+
+/*These are only in use when running the project locally */ 
 export const BASE_URLL = "http://localhost/hotel-api/establishments.json";
-
 export const CONTACT_URLL = "http://localhost/hotel-api/contact.json";
-
 export const ENQUIRIES_URLL = "http://localhost/hotel-api/enquiries.json";
